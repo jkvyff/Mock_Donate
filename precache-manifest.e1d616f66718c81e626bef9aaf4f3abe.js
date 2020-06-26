eed2e47fbf4f6884656ca12cf3259da4f2dfa1ef
jkvyff <kristianvyff@gmail.com>
2020-06-25 23:09:57 -0700

    Updates

---
+++ precache-manifest.e1d616f66718c81e626bef9aaf4f3abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8517d8483e83f324b8d066716bc2c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fd417daff16500264d1",
+    "url": "/static/css/main.5ae3a48f.chunk.css"
+  },
+  {
+    "revision": "343e2b4ca44371113735",
+    "url": "/static/js/2.6e26b5cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e26b5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd417daff16500264d1",
+    "url": "/static/js/main.9b43f1e4.chunk.js"
+  },
+  {
+    "revision": "e07aeebc52c87784cf5e",
+    "url": "/static/js/runtime-main.92c11e83.js"
+  }
+]);
